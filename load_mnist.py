6dd64b65b35b23fc0cdb614459846e59782d5d57
Eric Chan <eachan10@gmail.com>
2022-01-26 22:38:50 -0800

    Create mnist dataset file reader

--- load_mnist.py
+++ load_mnist.py
@@ -1,18 +1,32 @@
-from mnist import MNIST
 import numpy as np
 
 
+def read_images(path):
+    with open(path, mode='rb') as f:
+        magic_number = f.read(4)
+        images = int.from_bytes(f.read(4), 'big')
+        rows = int.from_bytes(f.read(4), 'big')
+        cols = int.from_bytes(f.read(4), 'big')
+        pixels = np.fromfile(f, np.uint8)
+    return pixels.reshape([images,cols*rows])
+
+def read_labels(path):
+    with open(path, mode='rb') as f:
+        magic_number = f.read(4)
+        label_count = f.read(4)
+        labels = np.fromfile(f, np.uint8)
+    return labels
+
 def load_mnist():
-    mndata = MNIST('data')
-    images, labels = mndata.load_training()
-    t = []
-    for label in labels:
+    test_images = read_images('data/t10k-images-idx3-ubyte')
+    test_labels = read_labels('data/t10k-labels-idx1-ubyte')
+    training_images = read_images('data/train-images-idx3-ubyte')
+    training_labels = read_labels('data/train-labels-idx1-ubyte')
+    t= []
+    for label in training_labels:
         a = np.zeros(10)
         a[label] = 1
         t.append(a)
-    images = [np.array(i) for i in images]
-    training_data = list(zip(images, t))
-    test_images, test_labels = mndata.load_testing()
-    test_images = [np.array(i) for i in test_images]
+    training_data = list(zip(training_images, t))
     test_data = list(zip(test_images, test_labels))
     return training_data, test_data
